c4752a3b8ad59053127de2467ca5945c8e15790f
Ralph Bayer <RalphBayer@protonmail.com>
2020-03-02 17:52:32 +0000

    Add files via upload

---
+++ bymath/__init__.py
@@ -0,0 +1,11 @@
+"""
+    This is a python libary that contains 4 seperate packages.
+    These packages are:
+        graphing package
+        machine learning package
+        random generator package
+        tensor math package
+
+"""
+
+from .tensor import *
