cd6bed3a22d8e2236802d866c34e212ceae92f48
Ralph Bayer <RalphBayer@protonmail.com>
2020-03-10 15:47:10 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 import bymath as bm
 
-t1 = bm.Tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
-t2 = 1
+t1 = bm.Tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], shape=[2, 2, 2])
 
-def add(vals):
-    val1, val2 = vals
-    return val1 + val2
-
-t3 = bm.map_to_func(add, t1, t2, shape=[2, 2, 2])
+# Sigmoid function
+print(1/(1+bm.exp(-t1)))
 
 # TODO:
 #   IMPROVE tslices_to_aslice FUNCTION IN TENSOR CLASS.
-#   ADD MATH FUNCTIONS FOR TENSOR OBJECT.
+#   FINISH ADDING MATH FUNCTIONS FOR TENSOR OBJECT.
 #   ADD A tensor_print FUNCTION.
 #   ADD DATA TYPES.
-#   ADD INITIALIZATION TYPES.
+#   ADD INITIALIZATION FUNCTIONS FOR THE TENSOR CLASS.
+#   CYTHONIZE EVERYTHING
